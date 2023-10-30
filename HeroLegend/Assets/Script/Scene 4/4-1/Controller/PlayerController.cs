3ae504eb2da020793935b9049f6a9ecb61c48128
chk7082 <whgusrl7082@gmail.com>
2023-10-30 14:25:13 +0900

    Feat: 다리떨리아 미니게임 import #180 - 따로 제작하던 미니게임 import - scene 4-1로 추가 Related to : #180
    Former-commit-id: dfe3bc49126edba95aab235cef61a855797c6383
    Former-commit-id: e4036da6dc16f3f5570974b1e71f5fb8a080be2f
    Former-commit-id: 8cd0a08fe15aa17ec0adf3c1393e6ae418af2997

---
+++ HeroLegend/Assets/Script/Scene 4/4-1/Controller/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour   
+{
+    public static bool s_canPresskey = true;
+
+    TimingManager theTimingManager;
+
+    private void Start()
+    {
+        theTimingManager = FindObjectOfType<TimingManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (s_canPresskey)
+            {
+                // 판정 체크
+                theTimingManager.CheckTiming();
+            }
+        }
+    }
+}
