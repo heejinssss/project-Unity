b3ab4b7f028102acf0610f6b0cc8ec2ed54dfc39
chk7082 <whgusrl7082@gmail.com>
2023-10-31 18:00:44 +0900

    Feat: 플레이어 이동 구현 #180 - 플레이어 이동 로직 구현 Related to : #180
    Former-commit-id: ac7c0c025c4e83ebe6ea0a5ba9b7a33f6da634bf
    Former-commit-id: 0b617a8e9bea9cef51025ebafa53d10ee6985c8a
    Former-commit-id: 56f14e409d51142cda365c0713db64cacad50ee2

---
+++ HeroLegend/Assets/Script/Scene 4/4/PlayerMovement4.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement4 : MonoBehaviour
+{
+    // static public members
+    public static PlayerMovement4 instance;
+
+    // -----------------------------------------------------------------------------------------
+    // public members
+    public float moveSpeed = 4f;
+    public Rigidbody2D rb;
+
+    // -----------------------------------------------------------------------------------------
+    // private members
+    private Vector2 movement;
+    bool isHorizonMove;
+
+    Animator anim;
+    Vector3 dirVec;
+    GameObject scanObject;
+
+
+    // -----------------------------------------------------------------------------------------
+    // awake method to initialisation
+    void Awake()
+    {
+        instance = this;
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // -----------------------------------------------------------------------------------------
+    // Update is called once per frame
+    void Update()
+    {
+        // update members
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Check Button Down & Up
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        // Check Horizontal Move
+        if (hDown)
+            isHorizonMove = true;
+        else if (vDown)
+            isHorizonMove = false;
+        else if (hUp || vUp)
+            isHorizonMove = movement.x != 0;
+
+        // Animation
+        if (anim.GetInteger("hAxisRaw") != movement.x)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)movement.x);
+        }
+        else if (anim.GetInteger("vAxisRaw") != movement.y)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)movement.y);
+        }
+        else
+        {
+            anim.SetBool("isChange", false);
+        }
+
+        // Direction
+        if (vDown && movement.y == 1) // 상
+            dirVec = Vector3.up;
+        else if (vDown && movement.y == -1) // 하
+            dirVec = Vector3.down;
+        else if (hDown && movement.x == -1) // 좌
+            dirVec = Vector3.left;
+        else if (hDown && movement.x == 1) // 우
+            dirVec = Vector3.right;
+
+        // Scan Object
+        // 스페이스바 눌러서 스캔
+        if (Input.GetButtonDown("Jump") && scanObject != null)
+        {
+            Debug.Log("This is : " + scanObject.name);
+        }
+
+    }
+    // -----------------------------------------------------------------------------------------
+    // fixed update methode
+    void FixedUpdate()
+    {
+        // Move
+        Vector2 moveVec = isHorizonMove ? new Vector2(movement.x, 0) : new Vector2(0, movement.y);
+        rb.MovePosition(rb.position + moveVec * moveSpeed * Time.fixedDeltaTime);
+
+        // Ray
+        Debug.DrawRay(rb.position, dirVec * 0.8f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rb.position, dirVec, 0.8f, LayerMask.GetMask("Object"));
+
+        if (rayHit.collider != null)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+        {
+            scanObject = null;
+        }
+    }
+}
