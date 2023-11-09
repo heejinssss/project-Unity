0e83458e7b23a89ec3d163f82f83bc75b7ae2700
임혜지 <hyeji4775@naver.com>
2023-11-09 14:46:44 +0900

    Fix: 쿼리 버그
    Former-commit-id: e8a88f83c07094ae7fb78bd4ecf6f32fa9c9d815
    Former-commit-id: 777348fdf526b9e86863f43d25fa17460127c719
    Former-commit-id: e3d73faab89c60a3a836eca0aea69bc55e69ca0b

--- HeroLegend/Assets/Script/Ranking/PlayerRankingClass.cs
+++ HeroLegend/Assets/Script/Ranking/PlayerRankingClass.cs
@@ -1,17 +1,17 @@
 using System;
 
 /* 랭킹 출력할 플레이어 클래스 */
-public class PlayerClass
+public class PlayerRankingClass
 {
     private long rank;
     private string nickname;
-    private uint score;
-    private uint playTime;
+    private int score;
+    private int playTime;
     private DateTime saveTime;
 
-    public PlayerClass() { }
+    public PlayerRankingClass() { }
 
-    public PlayerClass(long rank, string nickname, uint score, uint playTime, DateTime saveTime)
+    public PlayerRankingClass(long rank, string nickname, int score, int playTime, DateTime saveTime)
     {
         this.rank = rank;
         this.nickname = nickname;
@@ -22,7 +22,7 @@ public class PlayerClass
 
     public long getRank() { return rank; }
     public string getNickname() { return nickname; }
-    public uint getScore() { return score; }
-    public uint getPlayTime() { return playTime; }
+    public int getScore() { return score; }
+    public int getPlayTime() { return playTime; }
     public DateTime getSaveTime() { return saveTime; }
 }
