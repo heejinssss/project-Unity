368b0fdf849a995ba80b413be9454d8ad7b20a6c
LimHyeji <hyeji4775@naver.com>
2023-11-13 09:35:50 +0900

    Fix: savetTime 삭제
    Former-commit-id: 635a4caf46040e2cb5d3becabbc0cc6830f0266c
    Former-commit-id: 6fb4bed7488972c555a8549447e1303b84ec50ac
    Former-commit-id: 5e17b645f81d2abf9ebb12dbe7985e7f851aa7fa

--- HeroLegend/Assets/Script/Ranking/PlayerRankingClass.cs
+++ HeroLegend/Assets/Script/Ranking/PlayerRankingClass.cs
@@ -7,22 +7,19 @@ public class PlayerRankingClass
     private string nickname;
     private int score;
     private int playTime;
-    private DateTime saveTime;
 
     public PlayerRankingClass() { }
 
-    public PlayerRankingClass(long rank, string nickname, int score, int playTime, DateTime saveTime)
+    public PlayerRankingClass(long rank, string nickname, int score, int playTime)
     {
         this.rank = rank;
         this.nickname = nickname;
         this.score = score;
         this.playTime = playTime;
-        this.saveTime = saveTime;
     }
 
     public long getRank() { return rank; }
     public string getNickname() { return nickname; }
     public int getScore() { return score; }
     public int getPlayTime() { return playTime; }
-    public DateTime getSaveTime() { return saveTime; }
 }
